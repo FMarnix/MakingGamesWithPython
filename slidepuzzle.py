ad5f4ea76181fa5b2cf65dfc791e5c81e903186f
cyberdan34 <lipe98t@gmail.com>
2021-02-23 22:58:51 +0000

    230221

---
+++ slidepuzzle.py
@@ -0,0 +1,12 @@
+# Slide Puzzle
+# This is a copy of the code
+# from the book Making Games with Python 
+# by Al Sweigart
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Create the constants (go ahead and experiment with different values)
+BOARDWIDTH = 4 # number of columns in the board
+BOARDHEIGHT = 4 # number of rows in the board
+TILESIZE = 80
