178d25815a646866fe59f54d97f3afb4b490a27f
cyberdan34 <lipe98t@gmail.com>
2021-03-29 13:01:18 +0100

    29032021

--- slidepuzzle.py
+++ slidepuzzle.py
@@ -10,3 +10,14 @@ from pygame.locals import *
 BOARDWIDTH = 4 # number of columns in the board
 BOARDHEIGHT = 4 # number of rows in the board
 TILESIZE = 80
+WINDOWWIGHT = 640
+WINDOWHEIGHT = 480
+FPS = 30
+BLANK = None
+
+#                   R    G    B
+BLACK =            (  0,    0,   0)
+WHITE =            (255,  255, 255)
+BRIGHTBLUE =       (  0,   50, 255)
+DARKTURQUOISE =    (  3,   54,  73)
+GREEN =            (  0,  204,   0)
